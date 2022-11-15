d407b50906f2398efce032d398661a8ead1910f6
Matteo Bonacini <matteo.bonna@gmail.com>
2022-11-15 16:02:13 +0100

    mods

--- scripts/build:report:1.sh
+++ scripts/build:report:1.sh
@@ -2,6 +2,6 @@
 mkdir -p ./build/docs/reports/1 &&
 export $(cat ./docs/reports/.env | xargs) &&
 lualatex -output-directory=./build/docs/reports/1 ./docs/reports/1/report.tex &&
-#bibtex ./build/docs/reports/1/report.aux &&
-lualatex -output-directory=./build/docs/reports/1 ./docs/reports/1/report.tex #&& # Yep. Needed for references.
-#lualatex -output-directory=./build/docs/reports/1 ./docs/reports/1/report.tex;   # Yep. Needed for bibtex.
+bibtex ./build/docs/reports/1/report.aux &&
+lualatex -output-directory=./build/docs/reports/1 ./docs/reports/1/report.tex && # Yep. Needed for references.
+lualatex -output-directory=./build/docs/reports/1 ./docs/reports/1/report.tex;   # Yep. Needed for bibtex.
