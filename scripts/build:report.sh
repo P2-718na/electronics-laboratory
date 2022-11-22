f2a7cac24723ec6969c9c254493ba7c682a7901a
Matteo Bonacini <matteo.bonna@gmail.com>
2022-11-22 20:34:14 +0100

    Add template for reports

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 mkdir -p ./build &&
-export $(cat ../.env | xargs) &&
-pdflatex -output-directory=./build ./report/report.tex &&
-bibtex ./build/report.aux &&
-pdflatex -output-directory=./build ./report/report.tex && # Yep. Needed for references.
-pdflatex -output-directory=./build ./report/report.tex;   # Yep. Needed for bibtex.
+cd report &&
+export $(cat ../../.env | xargs) &&
+pdflatex -output-directory=../build ./report.tex &&
+cd .. &&
+echo "1" &&
+bibtex ./build/report.aux &&     # Whoever wrote bibtex needs to just stop.
+cd report &&
+pdflatex -output-directory=../build ./report.tex && # Yep. Needed for references.
+pdflatex -output-directory=../build ./report.tex;   # Yep. Needed for bibtex.
