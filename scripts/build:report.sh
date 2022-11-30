9fae7804718367fba91b431b8fee0f5e8e67a3b1
Matteo Bonacini <matteo.bonna@gmail.com>
2022-11-30 18:39:39 +0100

    Add data

--- scripts/build:report.sh
+++ scripts/build:report.sh
@@ -2,10 +2,9 @@
 mkdir -p ./build &&
 cd report &&
 export $(cat ../../.env | xargs) &&
-pdflatex -output-directory=../build ./report.tex &&
+pdflatex -output-directory=../build ./report.tex;
 cd .. &&
-echo "1" &&
-bibtex ./build/report.aux &&     # Whoever wrote bibtex needs to just stop.
+bibtex ./build/report.aux;     # Whoever wrote bibtex needs to just stop.
 cd report &&
 pdflatex -output-directory=../build ./report.tex && # Yep. Needed for references.
 pdflatex -output-directory=../build ./report.tex;   # Yep. Needed for bibtex.
